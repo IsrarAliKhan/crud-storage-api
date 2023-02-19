9a760c6e52d3848cb79cb468bd0c9c05fa8ca885
Israr Ali <israr.ali@datumbrain.com>
2023-02-19 14:11:46 +0500

    Added Request And Response Models For Item

--- internal/item/models/requests/item.go
+++ internal/item/models/requests/item.go
@@ -1,1 +1,16 @@
 package requests
+
+import "fmt"
+
+type Item struct {
+	Id    uint64 `json:"id"`
+	Name  string `json:"name"`
+	Price uint64 `json:"price"`
+}
+
+func (i *Item) Validate() error {
+	if i.Id == 0 || i.Name == "" {
+		return fmt.Errorf("missing required field")
+	}
+	return nil
+}
--- internal/item/models/responses/item.go
+++ internal/item/models/responses/item.go
@@ -1,1 +1,7 @@
 package responses
+
+type Item struct {
+	Id    uint64 `json:"id"`
+	Name  string `json:"name"`
+	Price uint64 `json:"price"`
+}
