ffc8bf2dd4ef950d36dd764addde2136c22bf7a1
Israr Ali <israr.ali@datumbrain.com>
2023-02-19 14:30:42 +0500

    Implemented Services For Category

--- internal/category/services/category.go
+++ internal/category/services/category.go
@@ -1,1 +1,44 @@
 package services
+
+import (
+	"crud-storage-api/internal/category/models/orms"
+	services "crud-storage-api/shared/service"
+	"crud-storage-api/shared/storage"
+	"fmt"
+	"log"
+)
+
+type CategoryService struct {
+	strg storage.Medium
+}
+
+func NewCategoryService(strg storage.Medium) *CategoryService {
+	return &CategoryService{strg}
+}
+
+func (CategoryService) ServiceType() int { return services.CATEGORY_SERVICE }
+
+func (s *CategoryService) GetCategory(id uint64) (orms.Category, error) {
+	res, err := s.strg.Get(id)
+	log.Println(err)
+	if err != nil {
+		return orms.Category{}, err
+	}
+	category, ok := res.(orms.Category)
+	if !ok {
+		return orms.Category{}, fmt.Errorf("conversion failed")
+	}
+	return category, err
+}
+
+func (s *CategoryService) SaveCategory(m orms.Category) (uint64, error) {
+	return s.strg.Save(m)
+}
+
+func (s *CategoryService) UpdateCategory(m orms.Category) error {
+	return s.strg.Update(m)
+}
+
+func (s *CategoryService) DeleteCategory(id uint64) error {
+	return s.strg.Delete(id)
+}
